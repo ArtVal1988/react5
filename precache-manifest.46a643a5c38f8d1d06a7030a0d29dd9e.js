927f798d6b8d1e0f707fd184e9a8a1535c702928
ArtVal1988 <Artem.0969619296@gmail.com>
2019-05-05 09:37:34 +0300

    Updates

---
+++ precache-manifest.46a643a5c38f8d1d06a7030a0d29dd9e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "055cdab11316329cd75cd4063b20f415",
+    "url": "/react5-hw1/index.html"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react5-hw1/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "053d41dd5bb18920b13d",
+    "url": "/react5-hw1/static/js/main.0e430790.chunk.js"
+  },
+  {
+    "revision": "82574e7ea3bd9e9ea2a0",
+    "url": "/react5-hw1/static/js/runtime~main.e0c65238.js"
+  }
+]);
